c033b7c9c47884f912ea9554b3200609e16a3f91
RubaGarcia <rgm177@alumnos.unican.es>
2023-10-20 16:38:44 +0200

    comenzada practica2, falta el ultimo paso del alg

--- practica2/independencia_nodos.py
+++ practica2/independencia_nodos.py
@@ -22,26 +22,80 @@ def main():
     data = reader("grafo1.txt").get_data()
 
     #numero de nodos
-    nNodos = data[0]
-
+    nNodos = int(data[0])
+    
     #creo los nodos en el grafico
     for num in range(nNodos):
         grafo.add_node(num)
 
     #creo las aristas de los nodos
     for fila in range(1, len(data) - 1):
-        nodos = fila.split(",")
-        grafo.add_edge(nodos[0], nodos[1])
+        nodos = data[fila].split(",")
+        grafo.add_edge(int(nodos[0]), int(nodos[1]))
 
-    
+    #print_graph(grafo)    
 
     #independencia a comprobar
     independencia = data[-1].split(",")
-    inde1 = independencia[0]
-    inde2 = independencia[1]
-    listaobsevados = independencia[2]
+    nodo1 = int(independencia[0])
+    nodo2 = int(independencia[1])
+    observados = independencia[2][1:-1]
+    lista_observados = []
+    for elem in observados.split("."):
+        lista_observados.append(int(elem))
 
+    lista_no_eliminables = lista_observados.copy()
+    lista_no_eliminables.append(nodo1)
+    lista_no_eliminables.append(nodo2)
+    
+    #print_graph(grafo)
 
+    eliminacion_hojas(grafo,lista_no_eliminables)
+
+    print_graph(grafo)
+
+    nuevo_grafo = union_padres(grafo)
+
+    print_graph(nuevo_grafo)
+
+def eliminacion_hojas(grafo, lista_no_eliminables):
+    print("Eliminacion de los vertices hoja")
+
+    #1. Eliminacion de los vertices hoja
+    nodosHoja = []
+
+    for nodo in grafo:
+        #grafo.edges te devuelve el nodo actual y el hijo
+        if len(grafo.edges(nodo)) == 0:
+            nodosHoja.append(nodo)
+
+    while(nodosHoja):
+        nodo = nodosHoja.pop()
+        if (nodo not in lista_no_eliminables):
+            padres = grafo.predecessors(nodo)
+            for padre in padres:
+                if padre not in nodosHoja:
+                    nodosHoja.append(padre)
+            grafo.remove_node(nodo)
+
+def union_padres(grafo):
+    
+    #Se une todos los padres con hijos comunes
+    for nodo in grafo:
+        padres = list(grafo.predecessors(nodo))
+        uniones = []
+        for i in range(len(padres)):
+            for j in range(len(padres[i:])):
+                if i != j:
+                    uniones.append((padres[i],padres[j]))
+        grafo.add_edges_from(uniones)
+
+    #Convertir los grafos en no dirigidos
+    non_dir_grafo = nx.Graph()
+    non_dir_grafo.add_nodes_from(list(grafo))
+    non_dir_grafo.add_edges_from(grafo.edges)
+    
+    return non_dir_grafo
 
 
 
