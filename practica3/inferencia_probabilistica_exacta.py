24a96cc393f17fec494985d4a8d8683d866cb365
Javier Mier <126773109+ModuleWare@users.noreply.github.com>
2023-11-13 17:30:18 +0100

    Avanzado combinacion de factores

--- practica3/inferencia_probabilistica_exacta.py
+++ practica3/inferencia_probabilistica_exacta.py
@@ -1,4 +1,4 @@
-
+import re
 
 class reader:
     def __init__(self, file):
@@ -144,13 +144,91 @@ def main():
                 if factor.contiene_variable(elemento_eliminable):
                     factores_reducibles.append(factor)
 
+
+
+            while len(factores_reducibles) > 1:
+                factor1 = factores_reducibles.pop()
+                factor2 = factores_reducibles.pop()
+
+                mapa_nuevo_factor = {}
+
+                variables_comunes = factor1.get_variables()
+                var_aux = factor2.get_variables()
+                for var in variables_comunes:
+                    if var not in var_aux:
+                        variables_comunes.remove(var)
+
+                #TODO: Saber las variables no comunes
+
+                factor1_keys = factor1.get_valores().keys()
+                factor2_keys = factor2.get_valores().keys()
+
+                llave_maestra_1 = "^".join(["*" for i in range(len(factor1.get_variables()))]) + "$"
+                llave_maestra_2 = "^".join(["*" for i in range(len(factor2.get_variables()))]) + "$"
+
+                mapas_valores_1 = factor1.get_valores()
+                mapas_valores_2 = factor2.get_valores()
+
+                indices_factor1 = []
+                indices_factor2 = []
+
+                for var in variables_comunes:
+                    indices_factor1.append(factor1.get_variable_position(var.get_nombre()))
+                    indices_factor2.append(factor2.get_variable_position(var.get_nombre()))
+
+                #TODO
+
+                valores_actuales = [0 for i in range(len(variables_comunes))]
+
+                # Funcion de ayuda para remover caracteres de un string
+                def char_remover(string, indexes):
+                    for index in sorted(indexes, reverse=True):
+                        string = string[:index] + string[index+1:]
+                    return string
+
+                while True:
+                    #Iterar por todas las versiones de las posibles llaves
+                    for value in range(len(valores_actuales)-1,0,-1):
+                        if valores_actuales[value] == variables_comunes[value].get_valores():
+                            valores_actuales[value] = 0
+                            valores_actuales[value-1] += 1
+                    if valores_actuales[0] == factor1.get_variables()[0].get_valores():
+                        break
+
+                    #Obtener las llaves de los factores
+                    llave1 = llave_maestra_1.copy()
+                    llave2 = llave_maestra_2.copy()
+
+                    #Generar las llaves de busqueda de esta iteracion
+                    for i in range(len(valores_actuales)):
+                        llave1[indices_factor1[i]] = valores_actuales[i]
+                        llave2[indices_factor2[i]] = valores_actuales[i]
+
+                    llaves_1 = re.search(llave1, factor1_keys)
+                    llaves_2 = re.search(llave2, factor2_keys)
+
+                    for clave in llaves_1:
+                        for clave2 in llaves_2:
+                            nuevo_valor = mapas_valores_1[clave] * mapas_valores_2[clave2]
+                            #Generar la nueva clave y guardar el valor
+                            nueva_clave = ""
+                            for num in valores_actuales:
+                                nueva_clave += str(num)
+                            nueva_clave += char_remover(clave, indices_factor1) + char_remover(clave2, indices_factor2)
+                            mapa_nuevo_factor.update({nueva_clave:nuevo_valor})
+
+                    #Avanzar al siguiente valor
+                    valores_actuales[-1] += 1
+
+                #TODO: Generar el nuevo factor y meterlo en la lista de factores reducibles
+
+
+            #TODO: Marginalizar el factor resultante
+
+
             variables_comunes = factores_reducibles[0].get_variables()
 
-            for factor in factores_reducibles[1:]:
-                variables_factor = factor.get_variables()
-                for var in variables_comunes:
-                    if not var in variables_factor:
-                        variables_comunes.remove(var)
+            
 
             
         #TODO 
