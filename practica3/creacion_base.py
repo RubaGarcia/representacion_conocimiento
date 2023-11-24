100ccbae9bd034b60a42c1564ed2af63dfe86328
RubaGarcia <rgm177@alumnos.unican.es>
2023-11-24 14:46:25 +0100

    anhadidos fichs final de practica
    anhadidos grafos de complejidad temporal, su script, los resultados y iniciada  practica4

---
+++ practica3/creacion_base.py
@@ -0,0 +1,49 @@
+#write in file
+import random
+import sys
+
+
+
+
+
+def generar_letras_no_repetidas(num):
+    letras = "abcdefghiJKLMNOpqrstuvwxyzÁÉÍÓÚáéíóúÜüÇçßàèìòùãõñĀāĒēĪīŌōŪū¿¡!@#$%^&*()-_=+[]<>?/"
+    letras=letras.lower()
+    letras_no_repetidas = random.sample(letras, min(num, len(letras)))
+    return letras_no_repetidas
+
+
+
+n = int(sys.argv[1])
+
+if len(sys.argv) >= 2:
+    f = open("base_tablas_" + str(sys.argv[1]) + ".txt", "w")
+    #quiero generar un array con n letras no repetidas
+    
+
+    letras = generar_letras_no_repetidas(n)
+
+    for i in letras[:-1]:
+        f.write(i + "2,")
+    f.write(letras[-1] + "2\n")
+
+
+    f.write(str(letras[0]) + "0:0.5\n" +
+        str(letras[0]) + "1:0.5\n")
+
+    for i in range(1, len(letras)):
+        f.write(str(letras[i]) + "0," + str(letras[i-1]) + "0:0.5\n" +
+            str(letras[i]) + "1," + str(letras[i-1]) + "0:0.5\n" +
+            str(letras[i]) + "0," + str(letras[i-1]) + "1:0.5\n" +
+            str(letras[i]) + "1," + str(letras[i-1]) + "1:0.5\n")
+        
+    
+   
+
+    for i in range(len(letras)-1):
+        f.write(str(letras[i].upper())+",")
+    f.write("\n")    
+    f.write("None\n")
+else:
+    print("Error: no se ha introducido el numero de la base")
+    exit()
--- practica3/script_tiempos.sh
+++ practica3/script_tiempos.sh
@@ -5,15 +5,18 @@ touch resultados.txt
 output_file="resultados.txt"
 
 # Bucle de 0 al 7
-for i in {0..6}; do
+for i in {1..62}; do
     # Llamar al programa Python con el parámetro del iterador
-    echo "Ejecutando fichero $i ..." >> $output_file
+    echo "Ejecutando fichero $i ..." 
 
-    { time python3 inferencia_probabilistica_exacta.py $i; } 2>> $output_file
+    python3 creacion_base.py $i  
 
+
+    python3 inferencia_probabilistica_exacta.py $i >> $output_file
     # Imprimir un separador en el archivo de salida
     
-    echo "------------------------" >> $output_file
+
+    #echo "------------------------" >> $output_file
 done
 
 echo "Proceso completado. Los resultados se han guardado en $output_file"
